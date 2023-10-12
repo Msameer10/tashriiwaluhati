dc52134ad1faa0b43f15ab5b5cc59f91dc183db4
Sameer <mohammadsameer0409@gmail.com>
2023-10-12 11:57:13 -0500

    initial commit

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-// h1 animation
-
-document.addEventListener("DOMContentLoaded", function () {
-    const title = document.querySelector(".animate-slide");
-    
-    // Add the "animate-slide" class after a short delay to trigger the animation
-    setTimeout(function () {
-        title.classList.add("animate-slide");
-    }, 500); // Adjust the delay (in milliseconds) as needed
-});
-
-
 // copyright year
 
 document.addEventListener("DOMContentLoaded", function () {
