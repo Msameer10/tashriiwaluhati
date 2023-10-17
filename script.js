8c54c83141bc264d2f644c333db4fe66e26684af
Sameer <mohammadsameer0409@gmail.com>
2023-10-17 00:43:24 -0500

    accordian

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const mainView = document.getElementById("main-view");
+    const rulesDiv = document.getElementById("rules");
+    const likesDiv = document.getElementById("likes");
+    const backToMainButtons = document.querySelectorAll("#back-to-main");
 
-  
+    function showDiv(divToShow) {
+        mainView.style.display = "none";
+        rulesDiv.style.display = "none";
+        likesDiv.style.display = "none";
+        divToShow.style.display = "block";
+    }
+
+    function showMainView() {
+        mainView.style.display = "block";
+        rulesDiv.style.display = "none";
+        likesDiv.style.display = "none";
+    }
+
+    backToMainButtons.forEach(function (button) {
+        button.addEventListener("click", function () {
+            showMainView();
+        });
+    });
+
+    document.querySelector(".left-div").addEventListener("click", function () {
+        showDiv(rulesDiv);
+    });
+
+    document.querySelector(".right-div").addEventListener("click", function () {
+        showDiv(likesDiv);
+    });
+});
+
+
 
 // copyright year
 
